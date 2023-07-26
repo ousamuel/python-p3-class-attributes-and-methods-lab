61a8c45a70c5dbdebd86e9886dd16c52041a2d9d
Samuel <samuelou510@gmail.com>
2023-07-25 20:26:37 -0400

    idk why i put genre:0 at first

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,23 @@
 class Song:
-    pass
+    count = 0
+    artists = []
+    genres = []
+    genre_count={}
+    artist_count={}
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        if (genre not in Song.genres):
+            Song.genres.append(genre)
+            Song.genre_count.update({genre : 1})
+        else:
+            Song.genre_count.update({genre: Song.genre_count[genre]+1})
+
+        if (artist not in Song.artists):
+            Song.artists.append(artist)
+            Song.artist_count.update({artist: 1})
+        else:
+            Song.artist_count.update({artist: Song.artist_count[artist] + 1})
+    
